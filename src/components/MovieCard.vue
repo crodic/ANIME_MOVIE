22357f8809a0cc6fd821fb4808e51bbbd3074967
crodic <alice01422@gmail.com>
2024-12-08 15:58:02 +0700

    update

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -20,7 +20,7 @@ const handleClick = ({ slug }: { slug: string }) => {
     @click="handleClick({ slug: data.slug })"
     v-if="data"
   >
-    <CardHeader class="p-0 h-[200px] md:h-[250px] relative">
+    <CardHeader class="p-0 w-full aspect-auto relative">
       <LazyImage
         :src="imgUrl + '/' + data?.thumb_url"
         :alt="data.origin_name"
